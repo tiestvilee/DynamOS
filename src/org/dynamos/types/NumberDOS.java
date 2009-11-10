3de55823e4e850c6edd7a416bceb21647a476b21
Tiest Vilee <git@tiestvilee.fastmail.com.au>
2009-11-10 10:53:30 +0000

    introduce how I would like the libraries to be included into the environment

---
+++ src/org/dynamos/types/NumberDOS.java
@@ -0,0 +1,10 @@
+package org.dynamos.types;
+
+import org.dynamos.structures.ObjectDOS;
+
+public class NumberDOS {
+
+	public static ObjectDOS createNumberFactory(ObjectDOS virtualMachine) {
+		return null;
+	}
+}
